ce58240eddcd3d779c81829be926e9b450edea1e
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2024-07-27 00:25:51 +0300

    Update to_adjacency_list.hpp

--- Transforming Graphs/to_adjacency_list.hpp
+++ Transforming Graphs/to_adjacency_list.hpp
@@ -1,10 +1,10 @@
 #include "../vertex.hpp"
 
-std::vector<std::vector<int> > get_adjacency_list(std::vector<Vertex*> g) {
-    std::vector<std::vector<int > > r;
+std::vector<std::vector<std::pair<int, int> > > get_adjacency_list(std::vector<Vertex*> g) {
+    std::vector<std::vector<std::pair<int, int> > > r;
     for (int i = 0; i<g.size(); i++) {
         for (int j = 0; j<g[i]->len(); j++) {
-            r[i].push_back(g[i]->adj(j)->val());
+            r[i].emplace_back(g[i]->adj(j)->val(), g[i]->weight(j));
         }
     }
     
